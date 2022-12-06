41d5573ea56cf5b6f90bcdc5f39a99d76b639b6f
Salman <salmanajani0426@gmail.com>
2022-12-06 19:14:06 +0530

    update: added navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,58 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY} sm:ml-32 xs:ml-20`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div className="flex flex-row items-center py-[8px] px-4 bg-discount-gradient rounded-[10px] mb-3">
+          <p className={`${styles.paragraph} text-gradient text-[24px]`}>
+            Bigger and Better
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" /> <span>Generation</span>{" "}
+          </h1>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          iPhone.
+        </h1>
+
+        <div className="ss:flex hidden ml-12 mt-4">
+          <GetStarted />
+        </div>
+      </div>
+
+      <div className={`ss:hidden ml-12 mt-6`}>
+        <GetStarted />
+      </div>
+
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
+        <img
+          src={robot}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
